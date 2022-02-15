2cdc0a654652e8f020b83191dfcc779afdf4ad01
Matias Andrada <matiasandrada5622@gmail.com>
2022-02-15 18:36:01 -0300

    primer commit

---
+++ script/Product_breakfast.js
@@ -0,0 +1,70 @@
+const coffee = [
+    {
+        "tipo": "Espresso doble",
+        "precio": "$40",
+    },
+    {
+        "tipo": "Cafe americano",
+        "precio": "$85",
+    },
+    {
+        "tipo": " Cappuccino",
+        "precio": "$160",
+    },
+    {
+        "tipo": "Cafe Latte",
+        "precio": "$65",
+    },
+    {
+        "tipo": "Latte de Vainilla",
+        "precio": "$65",
+    },
+    {
+        "tipo": "Latte de canela y especias",
+        "precio": "$85",
+    },
+    {
+        "tipo": "Latte de caramelo",
+        "precio": "$65",
+    },
+]
+const jugos = [
+    {
+        "tipo": "Naranja natural",
+        "precio": "$55",
+    },
+    {
+        "tipo": "Manzana",
+        "precio": "$65",
+    },
+    {
+        "tipo": "Fresa",
+        "precio": "$65",
+    },
+    {
+        "tipo": "Banana",
+        "precio": "$40",
+    }
+]
+const comida = [
+    {
+        "tipo": "Panes y huevos",
+        "precio": "$170",
+    },
+    {
+        "tipo": "Bizcocho salado",
+        "precio": "$15",
+    },
+    {
+        "tipo": "bizcocho con azucar",
+        "precio": "$15",
+    },
+    {
+        "tipo": "Burrito",
+        "precio": "$190",
+    },
+    {
+        "tipo": "Medialunas",
+        "precio": "$30",
+    }
+]
---
+++ script/admin.js
@@ -0,0 +1,15 @@
+const listDesayuno = coffee.length;
+console.log(listDesayuno)
+
+
+
+
+/*
+agregarCard(user){
+    const card = document.createElement("div")
+    card.classList.add("item--card")
+    card.innerHTML = usuario.getHTML(user.name, user.surname, user.email, user.telephone)
+    this.nodo.appendChild(card)
+    this.array.push(user)
+}
+*/
