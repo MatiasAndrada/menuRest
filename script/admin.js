8777dc5e6e4d707c9c8ebeb9053fb3102e40f969
Matias Andrada <matiasandrada5622@gmail.com>
2022-02-17 17:19:05 -0300

    BOTONES DE MENU

--- script/admin.js
+++ script/admin.js
@@ -2,41 +2,22 @@ const elmtCoffee = coffee.length;
 const elmtJugos = jugos.length;
 const elmtComida = comida.length;
 
+$("#breakfastBtn").click(() => {
+    new Producto(coffee);
 
-$(".btnDesayuno").click(() =>{ 
-
-for(const producto of coffee){
-    const itemProduct = $(".catalogo").append(`
-    <div class="item__product card-body">
-    <h2> Producto: ${producto.tipo} </h2>
-    <p> Precio: ${producto.precio} </p>
-    <div>
-`)
-    console.log(producto)
-}
-};
+})
 
 
-class product {
-    constructor(tipo, price){
-        this.tipo = tipo; 
-        this.price = price;
-    }
 
-    agregarProduct(product){
-        $(".catalogo").prepend(`
-        <h2>COMIDAAA<h2>
-        <p>parrafo<p>
-        `)
-    }
-}
 
-/*
-agregarCard(user){
-    const card = document.createElement("div")
-    card.classList.add("item--card")
-    card.innerHTML = usuario.getHTML(user.name, user.surname, user.email, user.telephone)
-    this.nodo.appendChild(card)
-    this.array.push(user)
-}
-*/
+
+
+/*    for(const producto of coffee){
+        $("#prdt").append(`
+            <div class="card-body col-3 m-1 bg-info text-white text-center">
+            <h3> ${producto.tipo} </h3>
+            <p> Precio: ${producto.precio} </p>
+            <button class="btn btn-success">+</button>
+            <div>`)
+        }*/
+
---
+++ script/product_class.js
@@ -0,0 +1,18 @@
+class Producto {
+    constructor(array){
+        this.array = array
+        this.pushCard()
+    }
+    
+    pushCard(){
+        for(const producto of this.array){
+            $("#prdt").append(`
+                <div class="card-body col-3 m-1 bg-info text-white text-center">
+                <h3> ${producto.tipo} </h3>
+                <p> Precio: ${producto.precio} </p>
+                <button class="btn btn-success">+</button>
+                <div>`)
+            }
+    }
+
+}
