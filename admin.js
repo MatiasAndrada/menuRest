40f35951423a4630903ea6f1acad28d966a78a7a
Matias Andrada <matiasandrada5622@gmail.com>
2022-02-20 17:21:54 -0300

    dsblBtn

--- admin.js
+++ admin.js
@@ -1,16 +1,27 @@
-
-//const prdt = new Producto(coffee);
-
-
-
-
-$(".buttonArea").on("click", function (evt){ 
-    if (evt.target.tagName === "BUTTON" ) {
-        const btn = evt;
-        
-
-        console.log(btn)
-
+$("#container-Dsyn").on("click", function (evt) {
+    if (evt.target.tagName === "BUTTON") {
+        const btn = evt.target;
+        const prdt = btn.dataset.array
+        switch (prdt) {
+            case "coffee":
+                new producto(coffee)
+                break;
+            case "jugos":
+                new producto(jugos)
+                break;
+            case "comida":
+                new producto(comida)
+                break;
+        }
     }
+    
+    function dsbldBtn () {
+        const btn = evt.target.id;
+        const btn2 = "#" + btn;
+        $(btn2).addClass("disabled")
+        console.log (btn)
+    }
+    dsbldBtn()
 })
+
 
--- components/product_class.js
+++ components/product_class.js
@@ -1,19 +1,19 @@
-class Producto {
+class producto {
     constructor(array){
-        this.array = array;
-        this.pushCard()
+        
+        this.pushCard(array)
     }
     
-    pushCard(){
-        for(const producto of this.array){
+    
+    pushCard(array){
+        for (const p of array){
             $("#prdt").append(`
                 <div class="card-body col-3 m-1 bg-info text-white text-center">
-                <h3> ${producto.tipo} </h3>
-                <p> Precio: ${producto.precio} </p>
+                <h3> ${p.tipo} </h3>
+                <p> Precio: ${p.precio} </p>
                 <button class="btn btn-success">+</button>
                 <div>`)
             }
-            
     }
 
 }
