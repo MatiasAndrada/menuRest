3ab300121eee34e6407862f2398581ba199b3afb
Matias Andrada <matiasandrada5622@gmail.com>
2022-03-05 21:09:48 -0300

    Carrito terminado

--- components/carrito.js
+++ components/carrito.js
@@ -4,22 +4,48 @@ function searchButtons() {
     addToCart.addEventListener("click", addToCartClicked);
   });
 }
+const comprarButton = document.querySelector(".comprarButton");
+comprarButton.addEventListener("click", comprarButtonClicked);
+
+
+const shoppingCartItemsContainer = document.querySelector(
+  ".shoppingCartItemsContainer"
+);
 
 function addToCartClicked(event) {
   const button = event.target;
   const item = button.closest(".card");
-  const itemTittle = item.querySelector(".card-tittle").textContent;
+  const itemTitle = item.querySelector(".card-title").innerText;
+
   const itemPrice = item.querySelector(".price").textContent;
   const itemImg = item.querySelector(".img").src;
-  addItemToCart(itemTittle, itemPrice, itemImg);
+  addItemToCart(itemTitle, itemPrice, itemImg);
 }
-function addItemToCart(itemTittle, itemPrice, itemImg) {
-  $(".shoppingCartItemsContainer").append(`
+
+function addItemToCart(itemTitle, itemPrice, itemImg) {
+  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+    "shoppingCartItemTitle"
+  );
+  for (let i = 0; i < elementsTitle.length; i++) {
+    if (elementsTitle[i].innerText === itemTitle) {
+      let elementQuantity = elementsTitle[
+        i
+      ].parentElement.parentElement.parentElement.querySelector(
+        ".shoppingCartItemQuantity"
+      );
+      elementQuantity.value++;
+      $('.toast').toast('show');
+      updatePriceTotal();
+      return;
+    }
+  }
+  const shoppingCartRow = document.createElement("div");
+  const shoppingCartContent = `
   <div class="row shoppingCartItem">
                     <div class="col-6">
                         <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                             <img src=${itemImg} class="shopping-cart-image">
-                            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTittle}
+                            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
                             </h6>
                         </div>
                     </div>
@@ -37,21 +63,50 @@ function addItemToCart(itemTittle, itemPrice, itemImg) {
                         </div>
                     </div>
                 </div>
-  `);
-  updatePriceTotal()
+  `;
+  shoppingCartRow.innerHTML = shoppingCartContent;
+  shoppingCartItemsContainer.append(shoppingCartRow);
+
+  shoppingCartRow
+    .querySelector(".buttonDelete")
+    .addEventListener("click", removeItemCart);
+
+  shoppingCartRow
+    .querySelector(".shoppingCartItemQuantity")
+    .addEventListener("change", quantityChanged);
+  updatePriceTotal();
 }
-function updatePriceTotal (){
+function updatePriceTotal() {
   let total = 0;
   const cartTotal = document.querySelector(".shoppingCartTotal");
   const cartItems = document.querySelectorAll(".shoppingCartItem");
-  cartItems.forEach(shoppingCartItem => {
-  const cartItemElement = shoppingCartItem.querySelector(".shoppingCartItemPrice");
-  const cartPriceItem = Number(cartItemElement.textContent.replace(
-    "$", ""))
-    const itemQuantity = shoppingCartItem.querySelector(".shoppingCartItemQuantity")
-    const itemQuantityValue = Number(itemQuantity.value)
+  cartItems.forEach((shoppingCartItem) => {
+    const cartItemElement = shoppingCartItem.querySelector(
+      ".shoppingCartItemPrice"
+    );
+    const cartPriceItem = Number(cartItemElement.textContent.replace("$", ""));
+    const itemQuantity = shoppingCartItem.querySelector(
+      ".shoppingCartItemQuantity"
+    );
+    const itemQuantityValue = Number(itemQuantity.value);
 
     total = total + cartPriceItem * itemQuantityValue;
-})
-cartTotal.innerHTML = `${total.toFixed(2)}$`
+  });
+  cartTotal.innerHTML = `${total.toFixed(2)}$`;
+}
+function removeItemCart(evt) {
+  const buttonCliked = evt.target;
+  buttonCliked.closest(".shoppingCartItem").remove();
+  updatePriceTotal();
+}
+function quantityChanged(evt) {
+  const input = evt.target;
+  if (input.value <= 0) {
+    input.value = 1;
+  }
+  updatePriceTotal();
+}
+function comprarButtonClicked () {
+  shoppingCartItemsContainer.innerHTML = "";
+  updatePriceTotal();
 }
---
+++ components/product_dinner.js
@@ -0,0 +1,38 @@
+const beer = [
+    {
+        "tipo": "Ipa",
+        "precio": "$40",
+        "img": "https://pulsocervecero.com/wp-content/uploads/2019/02/pulsocervecero_imperial_ipa_lata.jpg",
+    },
+    {
+        "tipo": "Red ipa",
+        "precio": "$85",
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjWbbKlY-DPauGWvLnQfjQNCyZr0E8O-Vh1Q&usqp=CAU",
+    },
+    {
+        "tipo": "Golden",
+        "precio": "$160",
+        "img": "https://https://untappd.akamaized.net/photos/2021_10_20/99020c8d06e92047127293178bd029df_640x640.jpg.net/wp-content/uploads/2020/11/Cappuccino.jpg",
+    },
+    {
+        "tipo": "Cafe Latte",
+        "precio": "$65",
+        "img": "https://cdn7.kiwilimon.com/recetaimagen/36986/640x426/th5-640x426-46349.jpg.webp",
+    },
+    {
+        "tipo": "Latte de Vainilla",
+        "precio": "$65",
+        "img": "https://comolopuedohacer.com/wp-content/uploads/2018/02/140405lrg.jpg",
+    },
+    {
+        "tipo": "Latte de canela y especias",
+        "precio": "$85",
+        "img": "https://previews.123rf.com/images/greenartphotography/greenartphotography1611/greenartphotography161100090/68628840-masala-t%C3%A8-chai-latte-tradizionale-caldo-indiano-teatime-cerimonia-dolce-latte-con-spezie-erbe-infusi.jpg",
+
+    },
+    {
+        "tipo": "Latte de caramelo",
+        "precio": "$65",
+        "img": "https://cdn7.kiwilimon.com/recetaimagen/36988/46353.jpg",
+    },
+]
